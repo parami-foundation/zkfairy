fb7211e4fc0ca31a78fc3d7c20a524800c2c4fe5
Hikaru Chang <i@rua.moe>
2024-03-15 01:11:59 +0800

    add: anti debug

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,19 @@ import { matchRoutes } from "@umijs/max";
 import { PROJECT_CONFIG } from "./constants/global";
 
 export function onRouteChange({ clientRoutes, location }: any) {
+  // ANTI DEBUGGER
+  document.oncontextmenu = function () {
+    return false;
+  };
+  function block() {
+    setInterval(() => {
+      Function('debugger')();
+    }, 50);
+  }
+  try {
+    block();
+  } catch (err) { }
+
   const route: any = matchRoutes(clientRoutes, location.pathname)?.pop()?.route;
   if (route) {
     document.title = route.title
